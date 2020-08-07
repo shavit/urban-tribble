f1e106cdd80323828e0fcd90d3fc1f413b65215c
shavit <shavit@users.noreply.github.com>
2020-08-07 09:53:13 -0400

    Complete the AMF types

--- src/main/cpp/amf.h
+++ src/main/cpp/amf.h
@@ -1,6 +1,16 @@
 #ifndef amf_h
 #define amf_h
 
+/*
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  | 16 bits | 16 bits       | header count*56 | 16 bits       | message count*64 |
+  |         |               | + bits          |               | +bits            |
+  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  |         |               |                 |               |                  |
+  | Version | Header Count  | Header Type     | Message Count | Message Type     |
+  |         |               |                 |               |                  |
+  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
 typedef enum {
     AMF0_NUMBER=0,
     AMF0_BOOL,
@@ -12,6 +22,37 @@ typedef enum {
     AMF0_REFERENCE,
     AMF0_ARRAY,
     AMF0_END,
+};
+
+/*
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  | 16 bits | 16 bits       | header count*56 | 16 bits       | message count*64 |
+  |         |               | + bits          |               | +bits            |
+  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  |         |               |                 |               |                  |
+  | Version | Header Count  | Header Type     | Message Count | Message Type     |
+  |         |               |                 |               |                  |
+  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+typedef enum {
+   AMF3_UNDEFINED=0,
+   AMF3_MULL,
+   AMF3_FALSE,
+   AMF3_TRUE,
+   AMF3_INTEGER,
+   AMF3_DOUBLE,
+   AMF3_STRING,
+   AMF3_XML,
+   AMF3_DATE,
+   AMF3_ARRAY,
+   AMF3_OBJECT,
+   AMF3_XML_END,
+   AMF3_BYTE_ARRAY,
+   AMF3_VECTOR_INT,
+   AMF3_VECTOR_UINT,
+   AMF3_VECTOR_DOUBLE,
+   AMF3_VECTOR_OBJECT,
+   AMF3_DICTIONARY
 };
 
 int stream_amf0(char* msg);
