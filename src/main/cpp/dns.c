78abcfc7439f8631cc33abd4c6d83211d67a933f
shavit <shavit@users.noreply.github.com>
2020-08-10 17:09:23 -0400

    Add dns files

---
+++ src/main/cpp/dns.c
@@ -0,0 +1,4 @@
+#include "dns.h"
+
+void addr_without_hostname(char* lhostname, char* lservice, addrinfo* lhints) {
+};
---
+++ src/main/cpp/dns.h
@@ -0,0 +1,8 @@
+#ifndef dns_h
+#define dns_h
+
+#include <netdb.h>
+
+void addr_without_hostname(char* lhostname, char* lservice, addrinfo* lhints) {};
+
+#endif
